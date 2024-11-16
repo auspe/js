8fe3a2bcd1124933ed4d399f6dd743a011ce6ad5
Alexander Uspensky <uspensky@live.de>
2024-11-16 19:06:21 +0300

    sorax

--- script.js
+++ script.js
@@ -30,3 +30,39 @@ console.log(result);
 
 alert(5); // обязательна точка с запятой
 [].push('a');
+
+let num = 20;
+
+function showFirstMessage(text) {
+  console.log(text);
+  let num = 10;
+  console.log(num);
+}
+
+showFirstMessage("Hello, Wordl!");
+console.log(num);
+
+function calc(a, b) {
+  return (a + b);
+}
+
+console.log(calc(2, 3));
+console.log(calc(1, 9));
+
+function ret() {
+  let num = 50;
+  return num;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+
+const logger = function() {
+  console.log("Hello, World");
+};
+
+logger();
+
+// стрелочная функция
+const calc = (a, b) => { return a + b };
+const calc2 = a => a;
