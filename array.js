050eda8168edc3daab116ca7ca55bcc988af4c5c
Alexander Uspensky <uspensky@live.de>
2025-01-14 14:27:09 +0300

    total2

---
+++ array.js
@@ -0,0 +1,12 @@
+arr2 = ["black", "orange", "pink"];
+
+arr3 = new Array(10); // => [ <10 empty items> ]
+arr4 = [10]; // [10]
+console.log(arr3);
+console.log(arr4);
+
+const arr = ["hi there"];
+arr[0] = "new value";
+console.log(arr[0]);
+
+arr = ["nope, now you are overwriting the array"];
---
+++ total2.js
@@ -0,0 +1,16 @@
+let firstNum = 5;
+let secondNum = 10;
+firstNum++;
+secondNum--;
+let total = ++firstNum + secondNum;
+console.log(total);
+let total2 = 500 + 100 / 5 + total--;
+console.log(total2);
+
+const a = 5;
+const b = 10;
+console.log(a > 0 && b > 0);
+console.log(a == 5 && b == 4);
+console.log(true || false);
+console.log(a == 3 || b == 10);
+console.log(a == 3 || b == 7);
