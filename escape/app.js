a6584fb6e907229653a46e74ad3d437b76d7be03
Alexander Uspensky u <aretmia@gmail.com>
2025-02-03 11:52:32 +0300

    Number

---
+++ escape/app.js
@@ -0,0 +1,9 @@
+let str = "Hello, what's your name? Is it \"Mike\"?";
+console.log(str);
+let str2 = 'Hello, what\'s your name? Is it "Mike"?';
+console.log(str2);
+
+let str3 = "New \nline.";
+let str4 = "I'm containing a backslash \\!";
+console.log(str3);
+console.log(str4);
--- number/script.js
+++ number/script.js
@@ -1,6 +1,15 @@
-'use strict';
+let intNr = 1;
+let decNr = 1.5;
+let expNr = 1.4e15;
+let octNr = 0o10;
+let hexNr = 0x3E8;
+let binNr = 0b101;
 
-let number = 4.6;
+console.log(expNr);
+console.log(octNr);
+console.log(hexNr);
+console.log(binNr);
 
-console.log(4/0);
-console.log('string' * 9);
+// Целые числа
+let intNr2 = 3434;
+let intNr3 = -111;
